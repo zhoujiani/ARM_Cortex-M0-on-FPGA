394c8b20401dc1020ad7cd9155d0c62d7344633b
zhoujiani <58023067+zhoujiani@users.noreply.github.com>
2024-10-25 16:15:30 +0800

    first commit

---
+++ keil/keil/pajiic.d
@@ -0,0 +1,7 @@
+.\pajiic.o: pajiic.c
+.\pajiic.o: pajiic.h
+.\pajiic.o: code_def.h
+.\pajiic.o: D:\Keil5\ARM\ARMCC\Bin\..\include\stdint.h
+.\pajiic.o: sflash.h
+.\pajiic.o: D:\Keil5\ARM\ARMCC\Bin\..\include\stdio.h
+.\pajiic.o: paj7620u2_cfg.h
---
+++ td/prj/CortexM0_SOC_Runs/syn_1/.read_design.begin.f
@@ -0,0 +1,5 @@
+<?xml version="1.0"?>
+<ProcessHandle Version="1" Minor="0">
+    <Process Ownner="POTATO" Host="LAPTOP-CEL0PSQP" Pid="17336">
+    </Process>
+</ProcessHandle>
